af6fd862a4ad8c8e28f814dbfe61934ab6a46537
Oleh Windows <oleh19127@gmail.com>
2021-09-20 01:03:21 +0300

    add feature(removes all empty folders)

--- Go/sort.go
+++ Go/sort.go
@@ -28,6 +28,8 @@ var (
 	torrent  = Data{"Torrent", "torrent"}
 	exe      = Data{"Exe", "exe"}
 	allData  = []Data{images, videos, music, document, psd, pdf, archive, torrent, exe}
+	// EMPTY FOLDERS
+	emptyFolders int
 )
 
 // GLOBAL FOLDER
@@ -46,26 +48,31 @@ func sorting() bool {
 		if err != nil {
 			fmt.Println(err)
 		}
+		// CALCULATE EMPTY FOLDERS
+		if info.IsDir() {
+			emptyFolders = emptyFolders + 1
+		}
 		// GET MODIFICATION FILE TIME
 		modTimeFolder := info.ModTime().Format("2006")
-		// SORTING
+		// CHECK FILES
 		for _, data := range allData {
 			extensions := strings.Split(data.extension, " ")
 			for _, extension := range extensions {
 				fileExtname := strings.Trim(filepath.Ext(info.Name()), ".")
+				// SORTING
 				if info.Name() != "sort_windows.exe" && info.Name() != "sort.exe" && strings.ToLower(fileExtname) == extension {
-					// IF FOLDER NOT EXIST CREATE
+					// IF FOLDERS NOT EXIST CREATE
 					if _, err := os.Stat(sortedFiles); os.IsNotExist(err) {
 						os.Mkdir(sortedFiles, 0755)
 						color.Yellow("Create " + sortedFiles + " folder")
 					}
 					if _, err := os.Stat(filepath.Join(sortedFiles, modTimeFolder)); os.IsNotExist(err) {
 						os.MkdirAll(filepath.Join(sortedFiles, modTimeFolder), 0755)
-						color.Yellow("Create " + modTimeFolder + " folder")
+						color.Yellow("Create " + sortedFiles + "/" + modTimeFolder + " folder")
 					}
 					if _, err := os.Stat(filepath.Join(sortedFiles, modTimeFolder, data.folder)); os.IsNotExist(err) {
 						os.Mkdir(filepath.Join(sortedFiles, modTimeFolder, data.folder), 0755)
-						color.Yellow("Create " + data.folder + " folder")
+						color.Yellow("Create " + sortedFiles + "/" + modTimeFolder + "/" + data.folder + " folder")
 					}
 					// MOVE FILE
 					os.Rename(path, filepath.Join(sortedFiles, modTimeFolder, data.folder, info.Name()))
@@ -99,14 +106,16 @@ func removeDir(path string, info os.FileInfo) {
 
 func main() {
 	if sorting() {
-		err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-			if info.IsDir() {
-				removeDir(path, info)
+		for i := 0; i < emptyFolders-1; i++ {
+			err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+				if info.IsDir() {
+					removeDir(path, info)
+				}
+				return nil
+			})
+			if err != nil {
+				fmt.Println(err)
 			}
-			return nil
-		})
-		if err != nil {
-			fmt.Println(err)
 		}
 	} else {
 		color.Cyan("NOTHING TO SORT!!!")
