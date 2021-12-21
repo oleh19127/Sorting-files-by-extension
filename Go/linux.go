fc6c4207b81c77b76e0e4595e60c1e9aba23ecd0
oleh312 <oleh19127@gmail.com>
2021-12-21 14:54:04 +0200

    update priority func for linux

--- Go/linux.go
+++ Go/linux.go
@@ -8,7 +8,18 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func controllCpuPriorityLinux() {
-	fmt.Println("Set Priority: Low,", "you can feel free in work!!!")
-	unix.Setpriority(unix.PRIO_PROCESS, 0, 5)
+func setCpuPriorityLinux(priority int) {
+	var priorityText string
+	if priority <= 20 && priority > 10 {
+		priorityText = "Very low"
+	} else if priority <= 10 && priority > 0 {
+		priorityText = "Low"
+	} else if priority <= -10 && priority >= -20 {
+		priorityText = "Hight"
+	} else {
+		priorityText = "Normal"
+		priority = 0
+	}
+	fmt.Println("Set Priority:", priorityText)
+	unix.Setpriority(unix.PRIO_PROCESS, 0, priority)
 }
