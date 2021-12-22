804e9fc204088d2c91dc30d1ded2bc85163b18c3
oleh312 <40184919+oleh312@users.noreply.github.com>
2021-12-22 21:59:38 +0200

    Update windows.go

--- Go/windows.go
+++ Go/windows.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 
 	"golang.org/x/sys/windows"
@@ -12,20 +11,20 @@ import (
 func setCpuPriorityWindows(priority string) {
 	// BELOW_NORMAL_PRIORITY_CLASS, NORMAL_PRIORITY_CLASS, ABOVE_NORMAL_PRIORITY_CLASS, HIGH_PRIORITY_CLASS
 	var priorityClass uint32
-	var priorityText string
+	// var priorityText string
 	if strings.ToLower(priority) == "below normal" {
-		priorityText = "Below normal"
+		// priorityText = "Below normal"
 		priorityClass = windows.BELOW_NORMAL_PRIORITY_CLASS
 	} else if strings.ToLower(priority) == "above normal" {
-		priorityText = "Above normal"
+		// priorityText = "Above normal"
 		priorityClass = windows.ABOVE_NORMAL_PRIORITY_CLASS
 	} else if strings.ToLower(priority) == "hight" {
-		priorityText = "Hight"
+		// priorityText = "Hight"
 		priorityClass = windows.HIGH_PRIORITY_CLASS
 	} else {
-		priorityText = "Normal"
+		// priorityText = "Normal"
 		priorityClass = windows.NORMAL_PRIORITY_CLASS
 	}
-	fmt.Println("Set Priority: ", priorityText)
+	// fmt.Println("Set Priority: ", priorityText)
 	windows.SetPriorityClass(windows.Handle(windows.CurrentProcess()), priorityClass)
 }
