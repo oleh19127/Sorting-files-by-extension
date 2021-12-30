e1550fdb1764a7e338f0c708137525a7d321a93f
oleh312 <40184919+oleh312@users.noreply.github.com>
2021-12-30 19:48:41 +0200

    add structure of extension and folders module

---
+++ Js/modules/structureOfExtension.mjs
@@ -0,0 +1,40 @@
+const allData = {
+  images: {
+    folder: 'Images',
+    extensions: ['png', 'jpg', 'webp', 'svg', 'gif', 'ico', 'jpeg', 'bmp', 'esp', 'jpeg 2000', 'heif', 'bat', 'cgm', 'tif', 'tiff', 'eps', 'raw', 'cr2', 'nef', 'orf', 'sr2']
+  },
+  videos: {
+    folder: 'Videos',
+    extensions: ['mp4', 'mov', 'wmv', 'fly', 'avi', 'mkv', 'flv', 'mpg', 'webm', 'oog', 'm4p', 'm4v', 'qt', 'swf', 'avchd', 'f4v', 'mpeg-2']
+  },
+  music: {
+    folder: 'Music',
+    extensions: ['mp3', 'aac', 'flac', 'alac', 'wav', 'aiff', 'dsd', 'pcm', 'm4a', 'wma']
+  },
+  documents: {
+    folder: 'Documents',
+    extensions: ['txt', 'doc', 'docx', 'docx', 'odt', 'xls', 'xlsx', 'ppt', 'pptx']
+  },
+  psd: {
+    folder: 'Psd',
+    extensions: ['psd']
+  },
+  pdf: {
+    folder: 'Pdf',
+    extensions: ['pdf']
+  },
+  archive: {
+    folder: 'Archive',
+    extensions: ['zip', 'rar', '7z', 'tar']
+  },
+  exe: {
+    folder: 'Exe',
+    extensions: ['exe']
+  },
+  torrent: {
+    folder: 'Torrent',
+    extensions: ['torrent']
+  },
+}
+
+export { allData }
