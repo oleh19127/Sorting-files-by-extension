2796fbb85e632646f9e074c615ffb96a40e85446
oleh312 <40184919+oleh312@users.noreply.github.com>
2022-07-18 22:18:21 +0300

    Create dirWrapper.services.js

---
+++ Js/services/dirWrapper.services.js
@@ -0,0 +1,20 @@
+import { existsSync, promises } from 'fs'
+import { print } from "./print.services.js"
+
+class DirWrapper {
+  async createDirIfNotExist(path) {
+    if (!existsSync(path)) {
+      await promises.mkdir(path, { recursive: true })
+      return print.warning(`Create: ${path}`)
+    }
+  }
+
+  async deleteDir(dir) {
+    await promises.rmdir(dir)
+    return print.error(`Folder removed: ${dir}`)
+  }
+}
+
+const dirWrapper = new DirWrapper
+
+export { dirWrapper }
